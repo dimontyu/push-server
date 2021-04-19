7e2fbc955768a06438b20aeab056978e1990abcd
dimontyu <43572676+dimontyu@users.noreply.github.com>
2021-04-19 23:08:49 +0300

    Update routecontrol.js
    подправил роутер

--- test-router/routecontrol.js
+++ test-router/routecontrol.js
@@ -1,6 +1,8 @@
 
+
 var express = require('express');
 var router = express.Router();
+const jsonParser = express.json();
 
 
 // Require our controllers.
@@ -12,21 +14,43 @@ var createbaza = require('../test-controller/createbaza');
 /// BOOK ROUTES ///
 
 
+
+
+
+
+
+
+
+// Require our controllers.
+
 router.get('/test', controller.index);//рендер страницы
 
 
-/* router.post('/test/create',controller.user_create_post);//создать нового жителя
 
 
-router.post('/city/create', controller.sity_create_post);//создать новый город
 
 
-router.post('/test/:id/delete', controller.user_delete_post);//удалить жителя
 
 
-router.post('/test/:id/update', controller.user_update_post);//обновить жителя
- */
+router.post("/test/user", jsonParser,controller.index_post );
+
+
+router.delete("/test/user/:id",controller.index_delete );
+
+router.put("/test/user", jsonParser,controller.index_put );
+
+
+
+
 router.post('/test/createbaza', createbaza.index_cites);//загрузить файл в базу данных
 router.get('/test/createbaza2', createbaza.index_citezen);//загрузить файл в базу данных
 
 module.exports = router;
+
+
+
+
+
+
+
+
